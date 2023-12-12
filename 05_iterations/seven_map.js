fe6ba69fb894c119ec73b5c2a0e859f60f597f17
Maaz Usmani <usmanimaaaz12@gmail.com>
2023-12-12 20:05:45 +0530

    Understanding DOM and its manipulation

--- 05_iterations/seven_map.js
+++ 05_iterations/seven_map.js
@@ -12,3 +12,18 @@ console.log(newNums);
 // important points about map
 // 1. map returns a new array
 // 2. map does not change the original array
+
+
+// Questtion : // What you have
+var officers = [
+  { id: 20, name: 'Captain Piett' },
+  { id: 24, name: 'General Veers' },
+  { id: 56, name: 'Admiral Ozzel' },
+  { id: 88, name: 'Commander Jerjerrod' }
+];
+// // What you need
+// [20, 24, 56, 88]
+
+// Answer:
+const ids = officers.map((off) => off.id)
+console.log(ids);

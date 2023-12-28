479abe9146af3f0b9a2af805744caca9010cb09b
Maaz Usmani <usmanimaaaz12@gmail.com>
2023-12-28 19:20:06 +0530

    OOP in js

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -1,21 +1,23 @@
-const user = {
-    username: "hitesh",
-    loginCount: 8,
-    signedIn: true,
+// const user = {
+//     username: "maaz",
+//     loginCount: 8,
+//     signedIn: true,
 
-    getUserDetails: function(){
-        //console.log("Got user details from database");
-        // console.log(`Username: ${this.username}`);
-        console.log(this);
-    }
+//     getUserDetails: function(){
+//         //console.log("Got user details from database");
+//         // console.log(`Username: ${this.username}`);
+//         console.log(this);  // this refers to the current object    
+//     }
 
-}
+// }
 
 
 
-//console.log(user.username)
-//console.log(user.getUserDetails());
-// console.log(this);
+// //console.log(user.username)
+
+// console.log(user.getUserDetails());
+
+// console.log(this);  // this refers to the global object (window object in browser and global object in nodejs)
 
 
 function User(username, loginCount, isLoggedIn){
@@ -31,7 +33,7 @@ function User(username, loginCount, isLoggedIn){
     return this
 }
 
-const userOne = new User("hitesh", 12, true)
-const userTwo = new User("ChaiAurCode", 11, false)
+const userOne = new User("maaz", 12, true)
+const userTwo = new User("John", 11, false)
 console.log(userOne.constructor);
-//console.log(userTwo);
+console.log(userTwo);

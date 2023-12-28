87961b760bd68d20ead3f27bf5950c8e6cfe8215
Maaz Usmani <usmanimaaaz12@gmail.com>
2023-12-28 19:52:33 +0530

    oop in js

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -5,7 +5,8 @@
 
 //     getUserDetails: function(){
 //         //console.log("Got user details from database");
-//         // console.log(`Username: ${this.username}`);
+              //  console.log(`Username: ${username}`);   // this will give error : username is not defined in this scope
+//         // console.log(`Username: ${this.username}`);     // "this" refers to the current object
 //         console.log(this);  // this refers to the current object    
 //     }
 
@@ -14,12 +15,12 @@
 
 
 // //console.log(user.username)
-
 // console.log(user.getUserDetails());
-
 // console.log(this);  // this refers to the global object (window object in browser and global object in nodejs)
 
 
+// NOTE : suppose we want to create 1000 users then we have to write 1000 times the same code for each user
+//       so we use constructor function to create multiple objects
 function User(username, loginCount, isLoggedIn){
     this.username = username;
     this.loginCount = loginCount;
@@ -32,6 +33,9 @@ function User(username, loginCount, isLoggedIn){
 
     return this
 }
+
+// Note :  If new is not used then this will create and change the same object again and again
+// Therefore new keyword creates an empty object and then calls the constructor function
 
 const userOne = new User("maaz", 12, true)
 const userTwo = new User("John", 11, false)

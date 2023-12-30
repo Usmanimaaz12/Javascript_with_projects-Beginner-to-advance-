7abfc23f1c17975f97de9f2e2ba92928847bfed2
Maaz Usmani <usmanimaaaz12@gmail.com>
2023-12-30 23:49:37 +0530

    getOwnPropertyDescriptor , define property

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -1,34 +1,35 @@
 const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
 
-console.log(descripter);
+console.log(descripter);    // { value: 3.141592653589793, writable: false, enumerable: false, configurable: false }
 
-// console.log(Math.PI);
-// Math.PI = 5
-// console.log(Math.PI);
+console.log(Math.PI);    // 3.141592653589793
+Math.PI = 5  // not change  value   because writable: false       
+console.log(Math.PI);   // 3.141592653589793
 
-// const chai = {
-//     name: 'ginger chai',
-//     price: 250,
-//     isAvailable: true,
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
 
-//     orderChai: function(){
-//         console.log("chai nhi bni");
-//     }
-// }
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
 
-// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));   // { value: 'ginger chai', writable: true, enumerable: true, configurable: true }
 
-// Object.defineProperty(chai, 'name', {
-//     //writable: false,
-//     enumerable: true,
+// Let make name property read only (Note : defineProperty is limited to only few) 
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true,
     
-// })
+})
 
-// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));  // { value: 'ginger chai', writable: false, enumerable: true, configurable: false }
 
-// for (let [key, value] of Object.entries(chai)) {
-//     if (typeof value !== 'function') {
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
         
-//         console.log(`${key} : ${value}`);
-//     }
-// }
+        console.log(`${key} : ${value}`);
+    }
+}
